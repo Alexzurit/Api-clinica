f079bb6f3516be2cba4863d47db6ea3ee0b85c8a
Alexzurit <alexeduardo.zuritajulca.3@gmail.com>
2025-05-01 19:07:22 -0500

    Proyeco subido

---
+++ src/main/java/azj/proyect/clinica/ClinicaApplication.java
@@ -0,0 +1,29 @@
+package azj.proyect.clinica;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.event.EventListener;
+
+import javax.sql.DataSource;
+import java.sql.SQLException;
+
+@SpringBootApplication
+public class ClinicaApplication {
+	//VERIFICAR AUTOMATICAMENTE LA CONEXION
+	@Autowired
+	private DataSource dataSource;
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClinicaApplication.class, args);
+	}
+
+	@EventListener(ApplicationReadyEvent.class)
+	public void testConnection() throws SQLException {
+		System.out.println("Conexion activa: " + dataSource.getConnection().isValid(2));
+		//Si es true (se conectó a la DB) de lo contrario seria false creo jajajja
+	}
+
+
+}
