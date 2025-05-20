6a3fefc9fa1de92b6358a3096bb99b91af680627
Alexzurit <alexeduardo.zuritajulca.3@gmail.com>
2025-05-19 23:34:19 -0500

    CRUD Doctor completo

--- src/main/java/azj/proyect/clinica/controller/DoctorController.java
+++ src/main/java/azj/proyect/clinica/controller/DoctorController.java
@@ -3,6 +3,7 @@ package azj.proyect.clinica.controller;
 import azj.proyect.clinica.entity.Doctor;
 import azj.proyect.clinica.service.DoctorService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -19,5 +20,22 @@ public class DoctorController {
     @PostMapping
     public Doctor crearDoctor(@RequestBody Doctor doctor){
         return doctorService.guardarDoctor(doctor);
+    }
+
+    @GetMapping("/{id}")
+    public Doctor obtenerDoctor(@PathVariable int id){
+        return doctorService.obtenerPorId(id)
+                .orElseThrow(() -> new RuntimeException("Doctor no encontrado"));
+    }
+
+    @PutMapping("/{id}")
+    public Doctor actualizarDoctor(@PathVariable int id, @RequestBody Doctor doctor){
+        return doctorService.actualizarDoctor(id, doctor);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity <String> eliminarDoctor(@PathVariable int id){
+        doctorService.eliminarDoctor(id);
+        return ResponseEntity.ok("Doctor Eliminado de forma correcta");
     }
 }
--- src/main/java/azj/proyect/clinica/service/DoctorService.java
+++ src/main/java/azj/proyect/clinica/service/DoctorService.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class DoctorService {
@@ -23,5 +24,33 @@ public class DoctorService {
                 .orElseThrow(() -> new RuntimeException("Especialidad no encontrada"));
         doctor.setEspecialidad(especialidad);
         return doctorRepository.save(doctor);
+    }
+
+    //Listar por id
+    public Optional <Doctor> obtenerPorId(int idDoctor){
+        return doctorRepository.findById(idDoctor);
+    }
+
+    //Actualizar doctor
+    public Doctor actualizarDoctor(int idDoctor, Doctor nuevoDoctor){
+        Doctor doctorExistente = doctorRepository.findById(idDoctor)
+                .orElseThrow(() -> new RuntimeException("Doctor no encontrado"));
+        doctorExistente.setNombres(nuevoDoctor.getNombres());
+        doctorExistente.setApellidoPat(nuevoDoctor.getApellidoPat());
+        doctorExistente.setApellidoMat(nuevoDoctor.getApellidoMat());
+        // Obtener especialidad desde la BD si se ha cambiado
+        if (nuevoDoctor.getEspecialidad() != null) {
+            Especialidad especialidad = especialidadRepository.findById(nuevoDoctor.getEspecialidad().getIdespecialidad())
+                    .orElseThrow(() -> new RuntimeException("Especialidad no encontrada"));
+            doctorExistente.setEspecialidad(especialidad);
+        }
+        return doctorRepository.save(doctorExistente);
+    }
+    //Eliminar Doctor
+    public void eliminarDoctor(int idDoctor){
+        if (!doctorRepository.existsById(idDoctor)){
+            throw new RuntimeException("Doctor no encontrado");
+        }
+        doctorRepository.deleteById(idDoctor);
     }
 }
