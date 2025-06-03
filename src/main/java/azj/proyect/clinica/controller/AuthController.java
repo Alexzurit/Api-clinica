8f3058da759e0b7de24603deb18036c09d3ff037
Alexzurit <alexeduardo.zuritajulca.3@gmail.com>
2025-06-03 14:55:17 -0500

    par agregar el main borre contenido de authcontroller y service

--- src/main/java/azj/proyect/clinica/controller/AuthController.java
+++ src/main/java/azj/proyect/clinica/controller/AuthController.java
@@ -1,21 +1,6 @@
 package azj.proyect.clinica.controller;
 
-import azj.proyect.clinica.service.AuthService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
 
-@RestController
-@RequestMapping("/api/auth")
 public class AuthController {
-    @Autowired
-    private AuthService authService;
 
-    @PostMapping("/login")
-    public String login(@RequestParam String email, @RequestParam String password) {
-        if (authService.autenticarUsuario(email, password)) {
-            return "Login exitoso";
-        } else {
-            return "Credenciales incorrectas";
-        }
-    }
 }
